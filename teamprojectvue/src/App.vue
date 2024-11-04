595ddd935973f04c534e4682afe35e649d2b4666
DESKTOP-SCJ83VA\user <leetorrl123@gmail.com>
2024-11-04 13:11:50 +0900

    1104

---
+++ teamprojectvue/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+<<<<<<< HEAD
+  <nav class="mx-0 bg-blue-300 shadow-md">
+    <div class="">
+      <div class="justify-center flex items-center ">
+        <RouterLink to="/">
+          <img src="./images/LOGO.png" alt="" class="w-32 max-md:w-28 ml-3">
+        </RouterLink>
+      </div>
+      <div class="flex text-white z-10 justify-end">
+=======
+  <nav class="relative mx-0">
+    <div class="flex justify-between">
+      <div class="z-10 flex items-center space-x-4">
+        <div class="w-1/6"><RouterLink to="/">
+          <img src="./images/LOGO.PNG" alt="" class="ml-3  max-md:w-28">
+        </RouterLink></div>
+      </div>
+      <div class="z-10 flex items-center text-white">
+>>>>>>> 75ed138c90830115b3902bbc544c506159be1a55
+        <div class="mr-2"><RouterLink to="/loginview">| 로그인</RouterLink></div>
+        <div class="mr-2"><RouterLink to="/jointermsofuse">| 회원가입</RouterLink></div>
+        <div class="mr-2"><RouterLink to="/vacationform">| 휴가신청</RouterLink></div>
+        <div class="mr-20"><RouterLink to="/laboview">| 연구실</RouterLink></div>
+      </div>
+<<<<<<< HEAD
+
+=======
+      <div class="absolute inset-0 h-40 bg-gradient-to-t from-blue-200">
+        <img class="w-full h-40 -z-30" src="../src/images/wbar_t.jpg" alt="" />
+      </div>
+>>>>>>> 75ed138c90830115b3902bbc544c506159be1a55
+    </div>
+  </nav>
+
+  <div class="m-5">
+    <div class="h-24"></div>
+
+    <RouterView />
+    <!-- 사이드배너 -->
+    <div class="" id="sidebann">
+      <div class="hidden">
+        <SideBanner />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SideBanner from './component/SideBanner.vue'
+</script>
+
+<style lang="scss" scoped></style>
